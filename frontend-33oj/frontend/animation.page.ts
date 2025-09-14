cb54c86711541112c532ca48ffce15081d6227bd
key8888 <aottware@gmail.com>
2025-09-14 18:31:20 +0900

    change logo lodatime

--- frontend-33oj/frontend/animation.page.ts
+++ frontend-33oj/frontend/animation.page.ts
@@ -174,7 +174,7 @@
 
   console.log("test from animation.page.ts");
 
-  window.addEventListener("load", () => {
+  window.addEventListener("loadstart", () => {
     const logoSelector =
       'img.nav__logo[src="/components/navigation/nav-logo-small_dark.png"]';
     const img = document.querySelector<HTMLImageElement>(logoSelector);
